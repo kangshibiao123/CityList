4dd0df073ddce662e455283297dd30bbb194f65c
歪歪1993 <978202378@qq.com>
2015-12-28 13:44:52 +0800

    Initial Commit

---
+++ CityList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CityList
+//
+//  Created by mc on 15/12/28.
+//  Copyright © 2015年 kangshibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CityList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityList
+//
+//  Created by mc on 15/12/28.
+//  Copyright © 2015年 kangshibiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
